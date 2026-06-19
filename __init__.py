71ad062ffd95cd904f4e64373c9107dcca2c0413
PozzettiAndrea <andrea@pozzetti.it>
2026-06-19 12:08:09 +0200

    Remove comfy_dynamic_widgets import
    The comfy_dynamic_widgets package is not a declared dependency and is not
    available; this unguarded import caused MeshSegmenter to fail to load.

--- __init__.py
+++ __init__.py
@@ -6,8 +6,5 @@ log.info("loading...")
 
 NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes()
 
-from comfy_dynamic_widgets import write_mappings
-write_mappings(NODE_CLASS_MAPPINGS, __file__)
-
 WEB_DIRECTORY = "./web"
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
